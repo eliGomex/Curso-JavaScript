054efe2381fe8deb4667f886748a08847f1c1da7
editel <editel463@gmail.com>
2024-06-04 15:44:29 -0300

    aula 14 incompleta

---
+++ aula14/number.js
@@ -0,0 +1,11 @@
+ let num1 = 10;
+ let num2 = 2.5;
+
+ // console.log(num1.toString + num2); concatenar 
+ // num1 = num1.toString(); transformar de number para string
+ // console.log(num1.toString(2)); binario representação
+ // console.log(num1.toFixed(2)); casas decimais
+ // console.log(Number.isInteger(num1)) verifica se é number
+ // let temp = num1 * 'ola';
+ // console.log(Number.isNaN(temp));
+ // 
