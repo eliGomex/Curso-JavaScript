43a6fe8929a16290c40a1f93658e84c2ec237eed
editel <editel463@gmail.com>
2024-06-10 15:46:17 -0300

    Adicionando aula 24 condicionais, aula 25 condicionais2 e modelo de pagina para as proximas aulas

---
+++ aula24/condicionais.js
@@ -0,0 +1,31 @@
+/*
+If pode ser usado sozinho
+Sempre que utilizo a palavra else, preciso de um if antes
+Eu posso ter varios els ifs na checagem
+Só posso ter um else na checagem
+podemos usar condições sem else if, utilizando apenas if e else
+*/
+
+
+// const hora = 50;
+
+// if (hora >= 0 && hora <= 11) {
+//     console.log('Bom dia');
+// }else if (hora >= 12 && hora <= 17){
+//     console.log('Boa Tarde')
+
+// }else if (hora >= 18 && hora <= 23){
+//     console.log('Boa Noite')
+
+    
+// }else {
+//     console.log('Olá');
+// }
+
+const tenhoGrana = true;
+
+if (tenhoGrana) {
+    console.log('Vou sair de casa');
+} else {
+    console.log('Não vou sair de casa')
+}
